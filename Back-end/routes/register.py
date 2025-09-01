bbf8b5338d7f981ba8cc57b90fd80375ac9ecefd
NishmithaBC <horimiyachan1995@gmail.com>
2025-09-01 22:28:41 +0530

    Update register.py

--- Back-end/routes/register.py
+++ Back-end/routes/register.py
@@ -1,6 +1,1 @@
-from fastapi import APIRouter,HTTPException,Depends
-from pydantic import BaseModel,Field
-from passlib.context import CryptContext
-from sqlalchemy.orm import Session
-from db import get_db
-from models import Student
+
